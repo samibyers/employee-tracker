973e7b2aff65d0d999cee3eda9688f6c31011a95
Sami Byers <samtaylor.byers@gmail.com>
2021-10-25 19:50:09 -0500

    semi-functional

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
-const { start } = require('repl');
+
 
 //comnnect to database
 const db = mysql.createConnection(
@@ -29,7 +29,7 @@ const db = mysql.createConnection(
 
 
 
-function startMenu() {
+const startMenu = () => {
     inquirer.prompt([
         {
             type: 'list',
@@ -48,7 +48,7 @@ function startMenu() {
             case "View all employees":
                 viewEmployees();
                 break;
-            case 'Add department':
+            case 'Add a department':
                 addDepartment();
                 break;
             case 'Add a role':
@@ -60,8 +60,8 @@ function startMenu() {
             case 'Add an updated employee role':
                 updateEmployeeRole();
                 break;
-            default:
-
+            case 'Quit':
+                quit(); 
                 break;
         };
     });
@@ -70,34 +70,37 @@ function startMenu() {
 startMenu();
 
 //displays the department table
-function viewDepartments() {
+const viewDepartments = () => {
     // Query database
     db.query('SELECT * FROM department', function (err, results) {
-    console.log(results);
-  });
-  startMenu();
+        console.log('\n');
+        console.table(results);
+        startMenu();
+    });
 };
 
 //dispays the role table
-function viewRoles() {
+const viewRoles = () => {
     // Query database
     db.query('SELECT * FROM role', function (err, results) {
-    console.log(results);
-  });
-  startMenu();
+        console.log('\n');
+        console.table(results);
+        startMenu();
+    });
 };
 
 //displays the employee table
-function viewEmployees() {
+const viewEmployees = () => {
     // Query database
     db.query('SELECT * FROM employee', function (err, results) {
-    console.log(results);
-  });
-  startMenu();
+        console.log('\n');
+        console.table(results);
+        startMenu();
+    });
 };
 
 //adds a new department to the department table
-function addDepartment() {
+const addDepartment = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -106,74 +109,90 @@ function addDepartment() {
         },
     ]).then(({name}) => {
         db.query('INSERT INTO department (name) VALUES (?)', name, (err, results) => {
-            console.log(results);
+            console.log('\n');    
+            console.table(results);
+            startMenu();
         });
-        startMenu();
     });
 };
 
-//adds a new role to the role table
-function addRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Enter the title of the role.',
-            name: 'title',
-        }, 
-        {
-            type: 'input',
-            message: 'Enter the salary of the role.',
-            name: 'salary',
-        }, 
-        {
-            type: 'list',
-            message: 'Which department does the role belong?',
-            name: 'department_id',
-            choices: []//choose from existing departments
-        }, 
-    ]).then(({title, salary, department_id}) => {
-        db.query('INSERT INTO role (title, salary, department_id) VALUES (?)', title, salary, department_id,  (err, results) => {
-            console.log(results);
-        });
-        startMenu();
+//creates an array of departments to pass into choices
+const departmentsArr = [];
+const chooseDepartment = () => {
+    db.query('SELECT name FROM department', (err, results) => {
+        for (i = 0; i < results.length; i++) {
+        departmentsArr.push(results[i].name);  
+        };
     });
+};
+
+
+//adds a new role to the role table
+const addRole = () => {
+    inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter the title of the role.',
+                name: 'title',
+            }, 
+            {
+                type: 'input',
+                message: 'Enter the salary of the role.',
+                name: 'salary',
+            }, 
+            {
+                type: 'list',
+                message: 'Which department does the role belong?',
+                name: 'name',
+                choices: chooseDepartment(),//choose from existing departments
+            }, 
+        ]).then(({title, salary, name}) => {
+            db.query('INSERT INTO role (title, salary, name) VALUES (?,?,?)', title, salary, name,  (err, results) => {
+                console.log('\n');
+                console.table(results);
+                startMenu();
+            });
+        });
 };
 
 //adds a new employee to the employee table
-function addEmployee() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Enter the employee\'s first name.',
-            name: 'first_name',
-        }, 
-        {
-            type: 'input',
-            message: 'Enter the employee\'s last name.',
-            name: 'last_name',
-        }, 
-        {
-            type: 'list',
-            message: 'What is the employee\'s role.',
-            name: 'role',
-            choices: []//choose from existing roles
-        }, 
-        {
-            type: 'list',
-            message: 'Who is the employee\'s manager.',
-            name: 'manager',
-            choices: []// choose from existing managers
-        }, 
-    ]).then(({first_name, last_name, role, manager}) => {
-        db.query('INSERT INTO employee (first_name, last_name, role, manager) VALUES (?)', first_name, last_name, role, manager,  (err, results) => {
-            console.log(results);
+const addEmployee = () => {
+    db.query('SELECT * FROM role', (err, results) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter the employee\'s first name.',
+                name: 'first_name',
+            }, 
+            {
+                type: 'input',
+                message: 'Enter the employee\'s last name.',
+                name: 'last_name',
+            }, 
+            {
+                type: 'list',
+                message: 'What is the employee\'s role.',
+                name: 'role',
+                choices: results//choose from existing roles
+            }, 
+            {
+                type: 'list',
+                message: 'Who is the employee\'s manager.',
+                name: 'manager',
+                choices: results// choose from existing employees
+            }, 
+        ]).then(({first_name, last_name, role, manager}) => {
+            db.query('INSERT INTO employee (first_name, last_name, role, manager) VALUES (?,?,?,?)', first_name, last_name, role, manager,  (err, results) => {
+                console.log('\n');
+                console.table(results);
+                startMenu();
+            }); 
         });
-        startMenu();
-    });
+   });
 };
 
-//
-function updateEmployeeRole() {
+//updates an employee's role
+const updateEmployeeRole = () => {
     inquirer.prompt([
         {
             type: 'list',
@@ -189,8 +208,13 @@ function updateEmployeeRole() {
         }, 
     ]).then(({name, title}) => {
         db.query('INSERT INTO role (title) VALUES (?)', title, (err, results) => {
-            console.log(results);
+            console.log('\n');
+            console.table(results);
+            startMenu();
         });
-        startMenu();
     });
+};
+
+const quit = () => {
+    console.log('To quit this application, press "control C" on your keyboard.');
 };
